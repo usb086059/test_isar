95946ee6426d9544c7de0fcd47d133cbd6933125
Robinson Salazar <usb086059@gmail.com>
2024-02-29 01:51:59 -0400

    conectado al proyecto en firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,15 @@ import 'package:flutter_application_1/app_router.dart';
 //import 'package:flutter_application_1/home_screen.dart';
 import 'package:flutter_application_1/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+//importaciones de Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Services().db;
   runApp(const ProviderScope(child: MyApp()));
 }
